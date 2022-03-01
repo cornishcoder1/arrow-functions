03994dc44ff73f06de1ae1ef3643d7725d0f143c
cornishcoder1 <leahfisher07@hotmail.co.uk>
2022-03-01 19:47:39 +0000

    push arrow function examples to repo

--- arrow-functions.js
+++ arrow-functions.js
@@ -3,12 +3,39 @@
  * command node arrow-functions.js in the terminal
  */
 
-// Vanilla JavaScript Function
+// // Vanilla JavaScript Function
+// function addTwoNumbers(a,b) {
+//     //Code block
+//     return a+b
+// }
+// let sum = addTwoNumbers(3,5);
+// console.log(sum);
 
 // Arrow Function With Parameters
+const addTwoNumbers = (a,b) => {
+    //Code block
+    return a+b
+}
+let sum = addTwoNumbers(3,5);
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
+// const addTwoNumbers2 = (a, b) => (a + b); //also valid
+const addTwoNumbers2 = (a, b) => a + b;
+let sum2 = addTwoNumbers2 (5,5);
+console.log(sum2)
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething('Hi, my name is Leah')
+
+const sayHello = () => console.log('hello');
+sayHello()
 
 // Returning Multiple Lines
+const returnMultipleLines = () => (
+    `<p> 
+        This is a multi-line string! 
+    <p>`
+)
+console.log(returnMultipleLines());
